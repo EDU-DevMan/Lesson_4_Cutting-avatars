1eea3db2b160f51984de0778e1edbef0b797ebbd
Ansle <89898234+ivan32rus@users.noreply.github.com>
2024-10-28 17:58:08 +0300

    Added parametrs image

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ from PIL import Image
 image = Image.open("images/example.jpg")
 rotated_image = image.rotate(45)
 rotated_image.save("images/rotated.jpg")
-# rotated_image.save("images\rotated.jpg", format="JPEG")
+width = image.width
+height = image.height
+cmyk_image = image.convert("CMYK")
+color_model = cmyk_image.mode
+
+print("""\
+      Ширина - {width}
+      Высота - {height}
+      Цветовая модель — {color_model} """.format(width=width, height=height, color_model=color_model))
