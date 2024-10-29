a51e49b8eab37b6a399857f2e991b765a50ef48e
Ansle <89898234+ivan32rus@users.noreply.github.com>
2024-10-29 11:53:14 +0300

    Removed unnecessary data

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from PIL import Image
 
-image = Image.open("images/example.jpg")
-rotated_image = image.rotate(45)
-rotated_image.save("images/rotated.jpg")
-width = image.width
-height = image.height
-cmyk_image = image.convert("CMYK")
-color_model = cmyk_image.mode
+image = Image.open("images/monro.jpg")
+red, green, blue = image.split()
+cut_left = 50   #   only an even number
+cut_middle = cut_left/2
+transparency = 0.3
 
-print("""\
-      Ширина - {width}
-      Высота - {height}
-      Цветовая модель — {color_model} """.format(width=width, height=height, color_model=color_model))
+cut_left_red = (cut_left, 0, image.width, image.height)
+cropped_left_red = red.crop(cut_left_red)
+cut_middle_red = (cut_middle, 0, image.width-cut_middle, image.height)
+cropped_middle_red = red.crop(cut_middle_red)
+mixing_red = Image.blend(cropped_left_red, cropped_middle_red, transparency)
+# image_red_middle = mixing_red.save("images/mixing_red.jpg")
+
+cut_right_blue = (0, 0, image.width-cut_left, image.height)
+cropped_right_blue = blue.crop(cut_right_blue)
+cut_middle_blue = (cut_middle, 0, image.width-cut_middle, image.height)
+cropped_middle_blue = blue.crop(cut_middle_blue)
+mixing_blue = Image.blend(cropped_right_blue, cropped_middle_blue, transparency)
+# image_blue_middle = mixing_blue.save("images/mixing_blue.jpg")
+
+cut_middle_green = (cut_middle, 0, image.width-cut_middle, 522)
+cropped_middle_green = green.crop(cut_middle_green)
+# cropped_middle_green.save("images/middle_green.jpg")
+
+new_image = Image.merge("RGB", (mixing_red, cropped_middle_green, mixing_blue))
+new_image.save("images/big_image.jpg")
+# print(new_image.size)
+new_image.thumbnail((80, 80))
+new_image.save("images/new_image.jpg")
+
